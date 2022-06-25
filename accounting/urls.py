87b48424085a5b4f6cbf002bf7a6c149ff2c2f09
salman_khosravipour <khosravipour.salman@gmail.com>
2022-06-25 18:14:25 +0430

    add forgot password

--- accounting/urls.py
+++ accounting/urls.py
@@ -1,4 +1,5 @@
-from django.urls import path
+from django.urls import path, reverse_lazy
+from django.contrib.auth import views as auth_view
 from accounting import views
 
 
@@ -8,5 +9,21 @@ urlpatterns = [
     path('sign-up/', views.sign_up, name='sign_up'),
     path('sign-in/', views.sign_in, name='sign_in'),
     path('sign-out/', views.sign_out, name='sign_out'),
-    path('password_reset', views.password_reset_request, name='password_reset'),
+
+    # Password reset urls
+    path('password_reset/', auth_view.PasswordResetView.as_view(
+        success_url=reverse_lazy('accounting:password_reset_done'),
+        email_template_name='accounting/password_reset_email.txt',
+        template_name='accounting/password_reset.html'), name='password_reset'),
+
+    path('password_reset_sent/', auth_view.PasswordResetDoneView.as_view(
+        # success_url=reverse_lazy('accounting:password_reset_confirm'),
+        template_name='accounting/password_reset_done.html'), name='password_reset_done'),
+
+    path('password_reset/<uidb64>/<token>/', auth_view.PasswordResetConfirmView.as_view(
+        success_url=reverse_lazy('accounting:password_reset_complete'),
+        template_name='accounting/password_reset_confirm.html'), name='password_reset_confirm'),
+    
+    path('password_reset_complete/', auth_view.PasswordResetCompleteView.as_view(
+        template_name='accounting/password_reset_complete.html'), name='password_reset_complete'),
 ]
--- core/settings.py
+++ core/settings.py
@@ -125,3 +125,12 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
+
+# SMTP config
+# EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = 'mailserver.django@gmail.com'
+EMAIL_HOST_PASSWORD = 'dcxtuzyynthxokwi'
