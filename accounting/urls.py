11fdbf9b81129db62b764b05d9fa241a70e733da
salman_khosravipour <khosravipour.salman@gmail.com>
2022-06-25 18:56:44 +0430

    add change password

--- accounting/urls.py
+++ accounting/urls.py
@@ -9,6 +9,13 @@ urlpatterns = [
     path('sign-up/', views.sign_up, name='sign_up'),
     path('sign-in/', views.sign_in, name='sign_in'),
     path('sign-out/', views.sign_out, name='sign_out'),
+    # Password change urls
+    path('password_change/', auth_view.PasswordChangeView.as_view(
+        success_url=reverse_lazy('accounting:password_change_done'),
+        template_name='accounting/password_change.html'), name='password_change'),
+
+    path('password_change/done/', auth_view.PasswordChangeDoneView.as_view(
+        template_name='accounting/password_change_done.html'), name='password_change_done'),
 
     # Password reset urls
     path('password_reset/', auth_view.PasswordResetView.as_view(
@@ -16,7 +23,7 @@ urlpatterns = [
         email_template_name='accounting/password_reset_email.txt',
         template_name='accounting/password_reset.html'), name='password_reset'),
 
-    path('password_reset_sent/', auth_view.PasswordResetDoneView.as_view(
+    path('password_reset/sent/', auth_view.PasswordResetDoneView.as_view(
         # success_url=reverse_lazy('accounting:password_reset_confirm'),
         template_name='accounting/password_reset_done.html'), name='password_reset_done'),
 
